079a8ad2053a840fd86b25bda19a77178d5d340c
Viki Val <viktorko99@gmail.com>
2025-02-28 12:27:30 -0700

    feat: add JWT authentication to drop API endpoint

--- server/api/drop/[chain]/[id].get.ts
+++ server/api/drop/[chain]/[id].get.ts
@@ -1,10 +1,24 @@
 import { $purify as purify } from "@kodadot1/minipfs";
 import { DateTime } from "luxon";
 import type { MemoDTO, Memo } from "~/types/memo";
+import { jwtVerify } from "jose/jwt/verify";
+import { decode } from "jose/base64url";
 
 const RUNTIME_CONFIG = useRuntimeConfig();
 export default defineEventHandler(async (event) => {
   const { id, chain } = getRouterParams(event);
+
+  const auth = getRequestHeader(event, "Authorization");
+  if (!auth) {
+    throw createError({
+      statusCode: 401,
+      message: "Unauthorized",
+    });
+  }
+
+  const JWT_SECRET = "secret";
+  const jwtKey = decode(JWT_SECRET);
+  const _token = await jwtVerify(auth.split(" ")[1], jwtKey);
 
   const [rawData, err] = await $fetch<MemoDTO>(`${RUNTIME_CONFIG.apiUrl}/poaps/detail/${chain}/${id}`)
     .then((r) => [r, null])
