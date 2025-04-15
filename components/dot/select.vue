691417aae105c4491c4de597838bd46f9a991f35
Branislav Hozza <brankohozza@gmail.com>
2025-04-15 19:02:11 +0200

    fix: better select

--- components/dot/select.vue
+++ components/dot/select.vue
@@ -1,26 +1,27 @@
 <template>
-  <div class="relative flex h-[45px] items-center">
-    <select
-      ref="selectInput"
-      v-model="model"
-      class="h-full w-full cursor-pointer appearance-none rounded-xl border border-border-default bg-transparent px-4 py-3 focus:outline-none"
-    >
-      <option v-for="option in options" :key="option.value" :value="option.value" :selected="option.value === model">
-        {{ option.text }}
-      </option>
-    </select>
-    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-      <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-      </svg>
-    </div>
-  </div>
+  <vue-select
+    v-model="model"
+    :classes="{
+      control: 'border h-[45px] !rounded-[12px] border-border-default gap-x-[12px] w-fit px-[19px]',
+    }"
+    :is-clearable="false"
+    :options="localOptions"
+  />
 </template>
 
 <script lang="ts" setup>
+import "vue-select/dist/vue-select.css";
+import VueSelect from "vue3-select-component";
 import type { Option } from "~/types/components";
 const model = defineModel<string>({ required: true });
-defineProps<{
+const props = defineProps<{
   options: Option[];
 }>();
+
+const localOptions = computed(() => {
+  return props.options.map((option) => ({
+    value: String(option.value),
+    label: option.text,
+  }));
+});
 </script>
