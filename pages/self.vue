b388dba59a6788706cdd6efcbafaf33758a30a24
Viki Val <viktorko99@gmail.com>
2025-09-02 19:08:57 +0200

    :construction: self qr testing

---
+++ pages/self.vue
@@ -0,0 +1,24 @@
+<template>
+  <ClientOnly>
+    <SelfQRcode
+      v-if="app"
+      :self-app="app"
+      :on-error="
+        () => {
+          console.log('Daco sa dojebalo');
+        }
+      "
+      :on-success="
+        () => {
+          console.log('Co ti jebe');
+        }
+      "
+    />
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+import { SelfQRcode } from "self-workspace-root/sdk/qrcode-vue";
+
+const { selfApp: app } = useSelf();
+</script>
