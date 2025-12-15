dc158f6915f71ed5a80e2e2fcaff7de0c7330c0d
hassnian <44554284+hassnian@users.noreply.github.com>
2025-12-15 11:46:28 +0500

    fix: claim page preview image not taking entire width

--- components/claim/ClaimPreview.vue
+++ components/claim/ClaimPreview.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="flex flex-col items-center gap-[32px]">
-    <image-preview :src="image" />
+  <image-preview :src="image" />
 
-    <div class="flex flex-col items-center gap-[24px]">
-      <h1 v-if="name" class="text-[31px] font-medium text-text-primary">
-        {{ customization?.heading || name }}
-      </h1>
-      <h2
-        v-if="description || customization?.subheading"
-        class="text-center text-[16px] font-normal !text-text-secondary"
-      >
-        {{ customization?.subheading || description }}
-      </h2>
-    </div>
-
-    <slot />
+  <div class="flex flex-col items-center gap-[24px]">
+    <h1 v-if="name" class="text-[31px] font-medium text-text-primary">
+      {{ customization?.heading || name }}
+    </h1>
+    <h2
+      v-if="description || customization?.subheading"
+      class="text-center text-[16px] font-normal !text-text-secondary"
+    >
+      {{ customization?.subheading || description }}
+    </h2>
   </div>
+
+  <slot />
 </template>
 
 <script setup lang="ts">
--- components/claim/ClaimSkeleton.vue
+++ components/claim/ClaimSkeleton.vue
@@ -1,31 +1,29 @@
 <template>
-  <div class="flex flex-col items-center gap-[32px]">
-    <!-- Image -->
-    <dot-skeleton class="h-[300px] w-[300px] rounded-[16px]" />
+  <!-- Image skeleton -->
+  <dot-skeleton class="h-52 w-full rounded-3xl md:h-72" />
 
-    <!-- Title and description -->
-    <div class="flex flex-col items-center gap-[24px]">
-      <dot-skeleton class="h-8 w-48 rounded" />
-      <dot-skeleton class="h-6 w-64 rounded" />
-    </div>
+  <!-- Title and description -->
+  <div class="flex flex-col items-center gap-[24px]">
+    <dot-skeleton class="h-8 w-48 rounded" />
+    <dot-skeleton class="h-6 w-64 rounded" />
+  </div>
 
-    <!-- Metadata -->
-    <div class="flex w-full flex-col gap-[12px]">
-      <dot-skeleton class="h-5 w-full rounded" />
-      <dot-skeleton class="h-5 w-full rounded" />
-    </div>
+  <!-- Metadata -->
+  <div class="flex w-full flex-col gap-[12px]">
+    <dot-skeleton class="h-5 w-full rounded" />
+    <dot-skeleton class="h-5 w-full rounded" />
+  </div>
 
-    <!-- Social links -->
-    <div class="flex gap-[8px]">
-      <dot-skeleton class="h-10 w-10 rounded-full" />
-      <dot-skeleton class="h-10 w-10 rounded-full" />
-      <dot-skeleton class="h-10 w-10 rounded-full" />
-    </div>
+  <!-- Social links -->
+  <div class="flex gap-[8px]">
+    <dot-skeleton class="h-10 w-10 rounded-full" />
+    <dot-skeleton class="h-10 w-10 rounded-full" />
+    <dot-skeleton class="h-10 w-10 rounded-full" />
+  </div>
 
-    <!-- Claim button -->
-    <div class="flex w-full flex-col space-y-[16px]">
-      <dot-skeleton class="h-12 w-full rounded-[12px]" />
-      <dot-skeleton class="h-8 w-32 self-center rounded" />
-    </div>
+  <!-- Claim button -->
+  <div class="flex w-full flex-col space-y-[16px]">
+    <dot-skeleton class="h-12 w-full rounded-[12px]" />
+    <dot-skeleton class="h-8 w-32 self-center rounded" />
   </div>
 </template>
