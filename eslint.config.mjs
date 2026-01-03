1355af6479e8b87d59e58a4aba86d22945cf433d
hassnian <44554284+hassnian@users.noreply.github.com>
2026-01-03 20:24:45 +0500

    chore: add antfu eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
+import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
-  {
-    rules:{
-      "vue/html-self-closing": "off",
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-    }
-  }
+  // @ts-expect-error - Type mismatch between FlatConfigComposer versions
+  antfu({
+    // ...@antfu/eslint-config options
+  }),
+  // ...your other rules
 )
